96c43a66e33e38a925925a5db8592dc5df2a43f3
Andrew Simmons <agsimmons0@gmail.com>
2019-01-15 03:41:02 +0000

    Start website application

--- ExploreYourCity/ExploreYourCity/urls.py
+++ ExploreYourCity/ExploreYourCity/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('website.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
