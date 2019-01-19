3b515a7982efd64e2b7f8f5877562e7a89016ae9
Andrew Simmons <agsimmons0@gmail.com>
2019-01-19 16:23:47 +0000

    Create Player model and tweak Location

--- ExploreYourCity/api/models.py
+++ ExploreYourCity/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
 # TODO: Add relationships between user and other tables
+
 
 class Region(models.Model):
     name = models.CharField(max_length=100)
@@ -15,7 +17,7 @@ class Category(models.Model):
 
 class Location(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category)
     latitude = models.FloatField()
     longitude = models.FloatField()
     # TODO: Add Google Maps related metadata like location ID or address
@@ -29,4 +31,12 @@ class Mission(models.Model):
 class Objective(models.Model):
     mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class Player(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    friends = models.ManyToManyField('self')
+    missions = models.ManyToManyField(Mission)
+    objectives_completed = models.ManyToManyField(Objective)
 
