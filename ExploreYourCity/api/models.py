194115114c92c6a78741c480c09d0d4344a9a855
Andrew Simmons <agsimmons0@gmail.com>
2019-01-19 16:00:56 +0000

    Created initial models for api

--- ExploreYourCity/api/models.py
+++ ExploreYourCity/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# TODO: Add relationships between user and other tables
+
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    radius = models.FloatField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # TODO: Add Google Maps related metadata like location ID or address
+
+
+class Mission(models.Model):
+    name = models.CharField(max_length=200)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
+class Objective(models.Model):
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
