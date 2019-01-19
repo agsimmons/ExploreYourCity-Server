924b196a52cbf7813ea6f40364065fa6867e8b92
Andrew Simmons <agsimmons0@gmail.com>
2019-01-19 20:15:28 +0000

    Enable rest_framework

--- ExploreYourCity/ExploreYourCity/settings.py
+++ ExploreYourCity/ExploreYourCity/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ExploreYourCity.wsgi.application'
+
+
+# Django REST Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 
 # Database
