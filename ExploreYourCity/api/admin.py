0614ae1d50786b4911563033b75683c8d7a57d57
Andrew Simmons <agsimmons0@gmail.com>
2019-01-19 16:47:42 +0000

    Register models to admin site and define __str__

--- ExploreYourCity/api/admin.py
+++ ExploreYourCity/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Region)
+admin.site.register(models.Category)
+admin.site.register(models.Location)
+admin.site.register(models.Mission)
+admin.site.register(models.Objective)
+admin.site.register(models.Player)
+
--- ExploreYourCity/api/models.py
+++ ExploreYourCity/api/models.py
@@ -10,9 +10,15 @@ class Region(models.Model):
     longitude = models.FloatField()
     radius = models.FloatField()
 
+    def __str__(self):
+        return f'{self.name} - {self.latitude},{self.longitude} - Radius: {self.radius}km'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Location(models.Model):
@@ -22,16 +28,23 @@ class Location(models.Model):
     longitude = models.FloatField()
     # TODO: Add Google Maps related metadata like location ID or address
 
+    def __str__(self):
+        return f'{self.name} - {self.latitude},{self.longitude}'
 
 class Mission(models.Model):
     name = models.CharField(max_length=200)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.region} - {self.name}'
 
 
 class Objective(models.Model):
     mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Mission: {self.mission.name} - Location: {self.location.name}'
 
 class Player(models.Model):
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
@@ -39,4 +52,7 @@ class Player(models.Model):
     friends = models.ManyToManyField('self')
     missions = models.ManyToManyField(Mission)
     objectives_completed = models.ManyToManyField(Objective)
+
+    def __str__(self):
+        return self.user.username
 
