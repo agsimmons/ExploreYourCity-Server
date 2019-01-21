e45aa4bcb23c1fdc645e425bb711747399b178bb
Andrew Simmons <agsimmons0@gmail.com>
2019-01-21 11:32:28 -0500

    Move debug setting to config.py

--- ExploreYourCity/ExploreYourCity/settings.py
+++ ExploreYourCity/ExploreYourCity/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.debug
 
 ALLOWED_HOSTS = config.allowed_hosts
 
