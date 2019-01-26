1351a8b90ec5c50e12aac41cf4aa40f80a1d5026
Andrew Simmons <agsimmons0@gmail.com>
2019-01-26 16:21:46 -0500

    Enable website app and setup initial homepage

--- ExploreYourCity/ExploreYourCity/settings.py
+++ ExploreYourCity/ExploreYourCity/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = config.allowed_hosts
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
