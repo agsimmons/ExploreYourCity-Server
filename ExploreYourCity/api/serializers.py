6c40eb25284e179745fcc3a6f438ec04b7a69434
Andrew Simmons <agsimmons0@gmail.com>
2019-01-26 22:06:20 -0500

    Remove unused import

--- ExploreYourCity/api/serializers.py
+++ ExploreYourCity/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
-from . import models
 
 
 class UserSerializer(serializers.ModelSerializer):
