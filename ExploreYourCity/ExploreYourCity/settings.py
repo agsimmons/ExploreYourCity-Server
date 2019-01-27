3f267d92b7fe4c568dbb330752da09defe88d72e
Andrew Simmons <agsimmons0@gmail.com>
2019-01-26 22:02:56 -0500

    Add email settings

--- ExploreYourCity/ExploreYourCity/settings.py
+++ ExploreYourCity/ExploreYourCity/settings.py
@@ -96,6 +96,13 @@ REST_FRAMEWORK = {
 DATABASES = config.database_configuration
 
 
+# Email
+EMAIL_USE_TLS = config.EMAIL_USE_TLS
+EMAIL_HOST = config.EMAIL_HOST
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
+EMAIL_PORT = config.EMAIL_PORT
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
