8cea49802080ae2dbc6a25c84db945df1d2d3054
Andrew Simmons <agsimmons0@gmail.com>
2019-01-26 22:05:49 -0500

    Create register_user endpoint

--- ExploreYourCity/api/urls.py
+++ ExploreYourCity/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register_user/', views.UserRegister.as_view(), name='register_user'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- ExploreYourCity/api/views.py
+++ ExploreYourCity/api/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import rest_framework.permissions
+from . import serializers
 
-def index(request):
-    return HttpResponse('Hello, World! This is the index view for the api!')
 
+class UserRegister(APIView):
+    permission_classes = (rest_framework.permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            # TODO: Deactivate user account and send verification email which re-activates account
+            # user.is_active = False
+            # user.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
