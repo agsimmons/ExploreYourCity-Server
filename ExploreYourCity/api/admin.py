f8398cffa66c4c6d8235c9c3ed889a959bce7c83
Andrew Simmons <agsimmons0@gmail.com>
2019-01-26 23:17:25 -0500

    Add Type to admin interface

--- ExploreYourCity/api/admin.py
+++ ExploreYourCity/api/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Region)
+admin.site.register(models.Type)
 admin.site.register(models.Category)
 admin.site.register(models.Location)
 admin.site.register(models.Mission)
