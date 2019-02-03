73197541815de08d75f7ae72ad256047efa47511
Andrew Simmons <agsimmons0@gmail.com>
2019-02-03 15:47:46 -0500

    Remove unused imports

--- ExploreYourCity/api/models.py
+++ ExploreYourCity/api/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User as DjangoUser
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 class Region(models.Model):
     name = models.CharField(max_length=100)
