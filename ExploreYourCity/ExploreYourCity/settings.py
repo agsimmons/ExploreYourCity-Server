c8ccb2707b5db6a41a5fae34a0917b572463f874
Andrew Simmons <agsimmons0@gmail.com>
2019-03-02 19:33:08 -0500

    Disable pagination

--- ExploreYourCity/ExploreYourCity/settings.py
+++ ExploreYourCity/ExploreYourCity/settings.py
@@ -76,9 +76,6 @@ WSGI_APPLICATION = 'ExploreYourCity.wsgi.application'
 
 # Django REST Framework Configuration
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
