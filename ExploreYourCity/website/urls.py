00b36e386769cd4c1761c4975b0670848c0f6c31
Andrew Simmons <agsimmons0@gmail.com>
2019-03-26 09:23:11 -0400

    Add robots.txt

--- ExploreYourCity/website/urls.py
+++ ExploreYourCity/website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.http import HttpResponse
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('robots.txt',
+         lambda x: HttpResponse("User-Agent: *\nDisallow: /", content_type="text/plain"),
+         name="robots_file"),
 ]
 
